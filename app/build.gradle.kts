731aff467dd7d32ab8d9cb5b5851c540ee4fd679
Ceph <gwethcephas01@gmail.com>
2026-02-19 13:40:25 +0300

    build: update project configuration for AGP 9.0.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,8 +1,5 @@
-import org.jetbrains.kotlin.gradle.dsl.JvmTarget
-
 plugins {
     alias(libs.plugins.android.application)
-    alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.kotzilla)
     alias(libs.plugins.jetbrainsKotlinSerialization)
@@ -26,23 +23,19 @@ android {
 
     buildTypes {
         release {
-            isMinifyEnabled = false
+            isMinifyEnabled = true
+            isShrinkResources = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
+            signingConfig = signingConfigs.getByName("debug")
         }
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
-    }
-
-    kotlin {
-        compilerOptions {
-            jvmTarget.set(JvmTarget.JVM_11)
-        }
-
     }
 
     buildFeatures {
@@ -102,5 +95,3 @@ dependencies {
     //konfetti
     implementation(libs.konfetti)
 }
-
-
